435198b4107cbb666cedffc667b896e0b3390818
Rishabh Srivastav <rishabh@testpress.in>
2018-08-03 21:39:05 +0530

    Update Comment, Tags in models

--- messageboard/models.py
+++ messageboard/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import django_filters
+
 
 # Create your models here
 
@@ -22,25 +24,23 @@ class Tags(models.Model):
         return self.title
     
     
-class Comment(models.Model):
-    content = models.TextField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    created = models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return self.content
-    
-
 class post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     tag = models.ManyToManyField(Tags)
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     content = models.TextField()
     title = models.CharField(max_length=50)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
-    
 
+
+class Comment(models.Model):
+    Post = models.ForeignKey(post, on_delete=models.CASCADE)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    created = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
     
