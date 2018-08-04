37738f1813ce6ab0e99238999723dae9f7749425
Rishabh Srivastav <rishabh@testpress.in>
2018-08-05 01:21:44 +0530

    model for people app

--- People/models.py
+++ People/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class PersonType(models.Model):
     """Person type model."""
-    title = models.CharField()
+    title = models.CharField(max_length=50)
     slug = models.SlugField()
 
     def __str__(self):
@@ -18,14 +18,15 @@ class Person(models.Model):
         (1, 'Male'),
         (2, 'Female'),
     )
-    first_name = models.CharField()
-    last_name = models.CharField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     slug = models.SlugField()
     gender = models.PositiveSmallIntegerField(choices=GENDER_CHOICES, blank=True, null=True)
-    facepic = models.FileField(upload_to=('facebook/'), blank=True)
+    image = models.FileField(upload_to=('images/'), blank=True)
     birth_date = models.DateField()
     person_types = models.ManyToManyField(PersonType)
     website = models.URLField()
+    created = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.first_name
