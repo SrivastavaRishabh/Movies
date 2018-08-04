643640e710328cc4c3da8a22046d4d5191dbe92c
Rishabh Srivastav <rishabh@testpress.in>
2018-08-04 18:45:39 +0530

    Add Blog models

---
+++ Blog/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.db.models import permalink
+from tagging.fields import TagField
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    birthdate = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
+
+    # @permalink
+    # def get_absolute_url(self):
+    #     return ('blog_category_detail', None, {'slug': self.slug})
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        (1, _('Draft')),
+        (2, _('Public')),
+    )
+    title = models.CharField(max_length=50)
+    slug = models.SlugField()
+    author = models.ForeignKey(Author, blank=True, null=True)
+    body = models.TextField()
+    status = models.IntegerField(choices=STATUS_CHOICES, default=2)
+    created = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category, blank=True)
+    tags = TagField()
+    
+    def __self__(self):
+        return self.title
+
+    # @permalink
+    # def get_absolute_url(self):
+    #     return ('blog_detail', None, {
+    #         'slug': self.slug
+    #     })
