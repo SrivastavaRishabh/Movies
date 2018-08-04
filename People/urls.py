1f9c94742f667e33129aa3515da6722ab9803ce0
Rishabh Srivastav <rishabh@testpress.in>
2018-08-05 01:20:53 +0530

    Url dispatcher of people app

---
+++ People/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.conf.urls import url
+from People import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+
+urlpatterns = [
+    path('', views.PeopleListView.as_view(), name="people-index"),
+    path('People/add', views.add, name='addperson'),
+    path('<slug:slug>/', views.PersonDetails.as_view(), name='people-details'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
