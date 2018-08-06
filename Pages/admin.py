5d55bd0c7d1e7ea9a39244d20a1a5584ee160764
Rishabh Srivastav <rishabh@testpress.in>
2018-08-06 21:05:37 +0530

    Admin display panel for pages app

---
+++ Pages/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Page
+# Register your models here.
+
+class PrepSlugAdmin(admin.ModelAdmin):
+     prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Page, PrepSlugAdmin)
+
