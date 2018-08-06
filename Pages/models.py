19e7e8999c406fd670214688e1ef2a31d3ce8d13
Rishabh Srivastav <rishabh@testpress.in>
2018-08-06 21:29:43 +0530

    Fix error of slug in model of pages app

--- Pages/models.py
+++ Pages/models.py
@@ -5,7 +5,7 @@ from django.utils.text import slugify
 
 class Page(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     html_content = models.TextField()
     order = models.IntegerField()
     created = models.DateTimeField(auto_now_add=True)
@@ -13,6 +13,11 @@ class Page(models.Model):
     class Meta:
         ordering = ['order', 'id']
     
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+        super(Page, self).save(*args, **kwargs)
+
     def __str__(self):
         return self.title
 
