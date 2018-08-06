6167fcb0f0f81ef9dd17958df09539742d24e297
Rishabh Srivastav <rishabh@testpress.in>
2018-08-06 15:58:03 +0530

    Fix Tag issue in model of bookmarkapp

--- Bookmark/models.py
+++ Bookmark/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-from tagging.fields import TagField
+from taggit.managers import TaggableManager
+
 
 class Bookmarks(models.Model):
     name = models.CharField(max_length=100)
     url = models.URLField(max_length=100)
     description = models.CharField(max_length=100)
-    tags = TagField()
+    tags = TaggableManager()
     
     def __str__(self):
         return self.name
