0313a205e9ac2894f94d7534ef53bba066c0b3cc
Rishabh Srivastav <rishabh@testpress.in>
2018-08-06 17:44:36 +0530

    URL dispatcher of bookmark app

---
+++ Bookmark/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from Bookmark import views
+
+urlpatterns = [
+    path('', views.BookmarkIndex.as_view(),name='bookmark_index'),
+    path('Bookmark/add', views.AddBookmark.as_view(), name='bookmark_add'),
+    path('Bookmark/<int:pk>/update/', views.UpdateBookmark.as_view(), name='bookmark_update'),
+    path('Bookmark/<int:pk>/delete/', views.DeleteBookmark.as_view(), name='bookmark_delete'),
+]
