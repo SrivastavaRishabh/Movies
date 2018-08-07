cfbb04750c28b381b0b3d39f04ba6967943449eb
Rishabh Srivastav <rishabh@testpress.in>
2018-08-07 16:00:33 +0530

    To apply filter in books app

---
+++ Books/filter.py
@@ -0,0 +1,9 @@
+import django_filters
+from .models import Books
+
+
+class BookFilter(django_filters.FilterSet):
+   
+    class Meta:
+       model = Books
+       fields = ['genre','publisher','authors',]
