72665059e7708628337efb68895105cf2d9dc0c2
Rishabh Srivastav <rishabh@testpress.in>
2018-08-08 15:14:02 +0530

    Unit test for movies app

--- Movies/tests.py
+++ Movies/tests.py
@@ -12,3 +12,9 @@ class MovieListView(TestCase):
     def test_view_url_by_name(self):
         resp = self.client.get(reverse('movies_index'))
         self.assertEqual(resp.status_code, 200)
+    
+    def test_view_template(self):
+        resp = self.client.get(reverse('movies_index'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'Movies/movies_index.html')
+  
