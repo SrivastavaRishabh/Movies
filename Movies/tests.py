a8e4f9f747bf422d85514138290d955b8f837a93
Rishabh Srivastav <rishabh@testpress.in>
2018-08-08 15:06:53 +0530

    Unit test for movies app

--- Movies/tests.py
+++ Movies/tests.py
@@ -8,3 +8,7 @@ class MovieListView(TestCase):
     def test_view_url_exists_at_desired_location(self):
         resp = self.client.get('/Movies/')
         self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('movies_index'))
+        self.assertEqual(resp.status_code, 200)
