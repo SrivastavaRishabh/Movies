52ec203994be6db2eb50b3faba927ef49730b864
Rishabh Srivastav <rishabh@testpress.in>
2018-08-09 16:20:13 +0530

    Unit testing for music app

---
+++ Music/tests.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+# from moviesapp.models import Movie
+from django.urls import reverse
+
+
+class MusicListView(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        resp = self.client.get('/Music/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('music_index'))
+        self.assertEqual(resp.status_code, 200)
+    
+    def test_view_template(self):
+        resp = self.client.get(reverse('music_index'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'Music/music_index.html')
