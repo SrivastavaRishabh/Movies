6eaefd215cd8916a2b7c5ada0db350121af32a8a
Rishabh Srivastav <rishabh@testpress.in>
2018-08-10 14:54:11 +0530

    Remove messageboard

--- messageboard/filter.py
+++
@@ -1,9 +0,0 @@
-import django_filters
-from .models import *
-
-
-class TagFilter(django_filters.FilterSet):
-   
-    class Meta:
-       model = post
-       fields = ['tag']
--- messageboard/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import *
-
-
-class EntryForm(forms.ModelForm):
-
-    class Meta:
-        model = post
-        fields = ('title', 'content', 'tag', 'author')
